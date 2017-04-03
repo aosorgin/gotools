55ebe4f8ea2a8136adee7899fef88bcf1440d6bb
aosorgin <alexey.osorgin@gmail.com>
2017-04-03 12:30:14 +0300

    Implement change command. Support intervals for changing files. Add tests for intervals

--- tools/filegen/fglib/changer.go
+++ tools/filegen/fglib/changer.go
@@ -7,11 +7,22 @@ Brief:     Files changer for random generator
 
 package fglib
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
 /* Interval tools */
 
 type intervalValueType struct {
-	value    uint64 // value
-	obsolete bool   // if true, value stored in bytes otherwise in percents
+	value    int64 // value
+	obsolete bool  // if true, value stored in bytes otherwise in percents
 }
 
 // Intervals are present with 3 values [size not to modify; size to modify; size not to modify
@@ -23,28 +34,102 @@ type IntervalType struct {
 
 func GetFullInterval() IntervalType {
 	return IntervalType{
-		modify: intervalValueType{value: 100, obsolete: false},
+		notModify:      intervalValueType{obsolete: true},
+		modify:         intervalValueType{value: 100, obsolete: false},
+		notModifyUntil: intervalValueType{obsolete: true},
 	}
 }
 
 func GetEmptyInterval() IntervalType {
 	return IntervalType{
-		notModify: intervalValueType{value: 100, obsolete: false},
+		notModify:      intervalValueType{value: 100, obsolete: false},
+		modify:         intervalValueType{obsolete: true},
+		notModifyUntil: intervalValueType{obsolete: true},
 	}
 }
 
-func getObsoleteInterval(interval IntervalType, size uint64) IntervalType {
+func getObsoleteInterval(interval IntervalType, size int64) IntervalType {
 	result := interval
-	correctProc := func(v intervalValueType) {
+	makeObsolete := func(v *intervalValueType) {
 		if v.obsolete == false {
-			v.value = uint64(float64(v.value*size) / float64(100))
+			v.value = int64(float64(v.value*size) / float64(100))
 			v.obsolete = true
 		}
 	}
-	correctProc(result.notModify)
-	correctProc(result.modify)
-	correctProc(result.notModifyUntil)
+	makeObsolete(&result.notModify)
+	makeObsolete(&result.modify)
+	makeObsolete(&result.notModifyUntil)
 	return result
+}
+
+// Interval format [digit{,kK,mM,gG,%},*3]. First value to seek without modification.
+// The next one is to modify file. The third is for seeking.
+
+func ParseInterval(data string) (result IntervalType, err error) {
+	intervals := strings.Split(data, ",")
+	if len(intervals) < 2 || len(intervals) > 3 {
+		err = fmt.Errorf("Invalid interval format. Must be 2 values at least but not more than 3.")
+		return
+	}
+
+	processIntervalValue := func(serialized string, i *intervalValueType) error {
+		r := regexp.MustCompile("([\\d]*)([%kKmMgG]{0,1})").FindStringSubmatch(serialized)
+		if len(r) < 3 {
+			return fmt.Errorf("Invalid interval format for '%s'", serialized)
+		}
+		i.obsolete = true
+		value, err := strconv.Atoi(r[1])
+		if err != nil {
+			return err
+		}
+		if value < 0 {
+			return fmt.Errorf("Invalid interval format for '%s'. Must be positive.", serialized)
+		}
+		i.value = int64(value)
+
+		switch r[2] {
+		case "%":
+			i.obsolete = false
+			if i.value > 100 {
+				return fmt.Errorf("Invalid interval format for '%s'. Must be in [0;100].", serialized)
+			}
+		case "k":
+			i.value *= 1000
+		case "K":
+			i.value *= 1024
+		case "m":
+			i.value *= 1000 * 1000
+		case "M":
+			i.value *= 1024 * 1024
+		case "g":
+			i.value *= 1000 * 1000 * 1000
+		case "G":
+			i.value *= 1024 * 1024 * 1024
+		case "":
+		default:
+			panic(fmt.Errorf("Invalid postfix in regex"))
+		}
+		return nil
+	}
+
+	err = processIntervalValue(intervals[0], &result.notModify)
+	if err != nil {
+		return
+	}
+
+	err = processIntervalValue(intervals[1], &result.modify)
+	if err != nil {
+		return
+	}
+
+	if len(intervals) > 2 {
+		err = processIntervalValue(intervals[2], &result.notModifyUntil)
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
 
 /* Changer implementation */
@@ -69,6 +154,83 @@ func (c *Changer) Close() error {
 	return c.gen.Stop()
 }
 
-func (c *Changer) ModifyFiles() {
-	// TODO: implement
+func min(a int64, b int64) int64 {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func changeFile(path string, info os.FileInfo, c *Changer) error {
+	i := getObsoleteInterval(c.interval, info.Size())
+	if i.modify.value == 0 {
+		return nil
+	}
+
+	file, err := os.OpenFile(path, os.O_RDWR, 0755)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// TODO: Support reverse changing
+
+	size := info.Size()
+	for size > 0 {
+		if i.notModify.value > 0 {
+			if size < i.notModify.value {
+				break
+			}
+			file.Seek(i.notModify.value, io.SeekCurrent)
+			size -= i.notModify.value
+		}
+
+		writen, err := io.CopyN(file, c.gen, min(i.modify.value, size))
+		size -= writen
+		if err != nil {
+			return err
+		}
+
+		if i.notModifyUntil.value > 0 {
+			if size < i.notModifyUntil.value {
+				break
+			}
+			file.Seek(i.notModifyUntil.value, io.SeekCurrent)
+			size -= i.notModifyUntil.value
+		}
+
+		if c.once == true {
+			break
+		}
+	}
+	return nil
+}
+
+func (c *Changer) ModifyFiles() error {
+	completeSignal := make(chan bool)
+	filesProcessed := 0
+
+	go func() {
+		filepath.Walk(Options.Path, func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			e := changeFile(path, info, c)
+			filesProcessed += 1
+			return e
+		})
+		completeSignal <- true
+	}()
+
+	timeout := time.Tick(time.Second)
+	for {
+		select {
+		case <-timeout:
+			fmt.Printf("\rProcessed: %d        ", filesProcessed)
+		case <-completeSignal:
+			fmt.Printf("\rProcessed: %d        ", filesProcessed)
+			return nil
+		}
+	}
+	return nil
 }
---
+++ tools/filegen/fglib/changer_test.go
@@ -0,0 +1,123 @@
+package fglib
+
+/*
+Author:    Alexey Osorgin (alexey.osorgin@gmail.com)
+Copyright: Alexey Osorgin, 2017
+
+Brief:     Tests for intervals parsing
+*/
+
+import (
+	"fmt"
+	"testing"
+)
+
+func checkIntervalValue(t *testing.T, valueName string, i intervalValueType, value int64, obsolete bool) {
+	if i.value != value || i.obsolete != obsolete {
+		t.Error(fmt.Errorf("Failed for process '%s' with values: (%d, %t). Must be (%d, %t)",
+			valueName, i.value, i.obsolete, value, obsolete))
+	}
+}
+
+func TestParseObsoleteIntervalShort(t *testing.T) {
+	i, err := ParseInterval("1234,654")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 1234, true)
+	checkIntervalValue(t, "modify", i.modify, 654, true)
+}
+
+func TestParseObsoleteInterval(t *testing.T) {
+	i, err := ParseInterval("8953,480,30902")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 8953, true)
+	checkIntervalValue(t, "modify", i.modify, 480, true)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 30902, true)
+}
+
+func TestFailOnNegativeInterval(t *testing.T) {
+	_, err := ParseInterval("-8953,480,30902")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse negative interval"))
+	}
+
+	_, err = ParseInterval("8953,-480,30902")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse negative interval"))
+	}
+
+	_, err = ParseInterval("8953,480,-30902")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse negative interval"))
+	}
+}
+
+func TestParseNotObsoleteInterval(t *testing.T) {
+	i, err := ParseInterval("23%,10%,17%")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 23, false)
+	checkIntervalValue(t, "modify", i.modify, 10, false)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 17, false)
+}
+
+func TestFailToParseNotObsoleteIntervalWithInvalidValues(t *testing.T) {
+	_, err := ParseInterval("8953%,80%,3%")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse more then 100%% interval"))
+	}
+
+	_, err = ParseInterval("89%,840%,3%")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse more then 100%% interval"))
+	}
+
+	_, err = ParseInterval("89%,40%,378%")
+	if err == nil {
+		t.Error(fmt.Errorf("Successfully parse more then 100%% interval"))
+	}
+}
+
+func TestParseCombineInterval(t *testing.T) {
+	i, err := ParseInterval("23K,96%,17k")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 23*1024, true)
+	checkIntervalValue(t, "modify", i.modify, 96, false)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 17*1000, true)
+
+	i, err = ParseInterval("23%,96m,17M")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 23, false)
+	checkIntervalValue(t, "modify", i.modify, 96*1000*1000, true)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 17*1024*1024, true)
+
+	i, err = ParseInterval("23g,96G,17%")
+	if err != nil {
+		t.Error(err)
+	}
+	checkIntervalValue(t, "notModify", i.notModify, 23*1000*1000*1000, true)
+	checkIntervalValue(t, "modify", i.modify, 96*1024*1024*1024, true)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 17, false)
+}
+
+func TestGetFullInterval(t *testing.T) {
+	i := GetFullInterval()
+	checkIntervalValue(t, "notModify", i.notModify, 0, true)
+	checkIntervalValue(t, "modify", i.modify, 100, false)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 0, true)
+}
+
+func TestGetEmptyInterval(t *testing.T) {
+	i := GetEmptyInterval()
+	checkIntervalValue(t, "notModify", i.notModify, 100, false)
+	checkIntervalValue(t, "modify", i.modify, 0, true)
+	checkIntervalValue(t, "notModifyUntil", i.notModifyUntil, 0, true)
+}
