70835b1adb9be9fcb859b554555d7a122df9e9b6
Alexey Osorgin <alexey.osorgin@gmail.com>
2017-05-19 12:08:33 +0300

    Use optparse command lime parser instead of flag

--- tools/filegen/fglib/cmd_options.go
+++ tools/filegen/fglib/cmd_options.go
@@ -8,10 +8,11 @@ Brief:     Command-line options parser and logic
 package fglib
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"time"
+	"io"
+	"github.com/go2c/optparse"
 )
 
 // CommandEnum
@@ -72,7 +73,7 @@ func processCommonCommand() {
 
 	if len(Options.Path) == 0 {
 		fmt.Fprintf(os.Stderr, "Error: path is not set. Use the --path option.\n")
-		flag.Usage()
+		usage(os.Stderr)
 		os.Exit(1)
 	}
 }
@@ -80,13 +81,13 @@ func processCommonCommand() {
 func processGenerateCommand() {
 	if Options.Generate.Folders == 0 {
 		fmt.Fprintf(os.Stderr, "Error: Use the --folders option to set files count to generate.\n")
-		flag.Usage()
+		usage(os.Stderr)
 		os.Exit(1)
 	}
 
 	if Options.Generate.Files == 0 {
 		fmt.Fprintf(os.Stderr, "Error: Use the --files option to set files count to generate.\n")
-		flag.Usage()
+		usage(os.Stderr)
 		os.Exit(1)
 	}
 }
@@ -106,49 +107,119 @@ func processGeneratorType(genType string, seed uint64) {
 		fmt.Println("Using seed:", seed)
 	} else {
 		fmt.Fprintf(os.Stderr, "Error: invalid generator type '%s'.\n", genType)
-		flag.Usage()
+		usage(os.Stderr)
 		os.Exit(1)
 	}
 }
 
 func processCommand(cmd string) {
-	if cmd == "gen" {
+	if cmd == "gen" || cmd == "generate" {
 		Options.Command = CommandGenerate
 		processCommonCommand()
 		processGenerateCommand()
-	} else if cmd == "change" {
+	} else if cmd == "chg" || cmd == "change" {
 		Options.Command = CommandChange
 		processCommonCommand()
 	} else {
 		fmt.Fprintf(os.Stderr, "Error: Invalid command '%s'\n", cmd)
-		flag.Usage()
+		usage(os.Stderr)
 		os.Exit(1)
 	}
+}
+
+func usage(f io.Writer) {
+	fmt.Fprintln(f, "Usage:")
+	fmt.Fprintf(f, "  %s [command] [options]\n\n", os.Args[0])
+
+	fmt.Fprintln(f, "Commands:")
+	fmt.Fprintln(f, "  gen, generate              Generate files")
+	fmt.Fprintln(f, "  chg, change                Change files")
+	fmt.Fprintln(f)
+
+	fmt.Fprintln(f, "Common options:")
+	fmt.Fprintln(f, "  -p, --path                 Path to processing folder")
+	fmt.Fprintln(f)
+
+	fmt.Fprintln(f, "Generate command options:")
+	fmt.Fprintln(f, "  -d, --dirs                 Directories count to generate")
+	fmt.Fprintln(f, "  -f, --files                Files count to generate")
+	fmt.Fprintln(f, "  -s, --size                 File size to generate. Size format: [\\d{k,K,m,M,g,G}]")
+	fmt.Fprintln(f)
+
+	fmt.Fprintln(f, "Change command options:")
+	fmt.Fprintln(f, "  --scale                    Percent of files cont to change. Range: [0;1]. By default is equal to 1")
+	fmt.Fprintln(f, "  -i, --interval             Interval to change file with. Format: ['data not to change', 'data to change',{'data not to change'}].")
+	fmt.Fprintln(f, "                             Data format: [\\d{%, k,K,m,M,g,G}]. By default is [0,100%] and used until file ending")
+	fmt.Fprintln(f, "  --once                     Using of interval only once. Used only with -i, --interval option.")
+	fmt.Fprintln(f, "  --reverse                  Using interval from the file ending to begining. Used only with -i, --interval option.")
+	fmt.Fprintln(f)
+
+	fmt.Fprintln(f, "Generator options:")
+	fmt.Fprintln(f, "  -g, --generator            Type of generator to use")
+	fmt.Fprintln(f, "     crypto                  Crypto random data generator. Used by default.")
+	fmt.Fprintln(f, "     pseudo                  Pseudo random data generator")
+	fmt.Fprintln(f, "  --seed                     Initial seed for generated data. Can be used only with 'pseudo' generator")
+	fmt.Fprintln(f)
+
+	fmt.Fprintln(f, "Auxillary options:")
+	fmt.Fprintln(f, "  -h, --help                 Print help and exit")
+	fmt.Fprintln(f, "  -v, --version              Print version and exit")
 }
 
 func ParseCmdOptions() {
 	/* Initializing flags for parsing command-line arguments */
 
-	flag.UintVar(&Options.Generate.Files, "files", 0, "Number of files to generate")
-	flag.UintVar(&Options.Generate.Folders, "folders", 1, "Number of folders to generate")
-	fileSize := flag.String("size", "0", "Size of files to generate")
+	/* generate command options */
+	optparse.UintVar(&Options.Generate.Files, "files", 'f', 0)
+	optparse.UintVar(&Options.Generate.Folders, "dirs", 'd', 1)
+	fileSize := optparse.String("size", 's', "0")
 
-	flag.Float64Var(&Options.Change.Ratio, "change-ratio", float64(1), "Change ratio (files count). Could be in [0;1]")
-	flag.BoolVar(&Options.Change.Once, "once", false, "Process interval only once. By default is false")
-	flag.BoolVar(&Options.Change.Reverse, "reverse", false, "Process interval from the end of file. By default is false")
-	interval := flag.String("interval", "", "Interval to change file. By default do not changes the file")
+	/* change command option */
+	optparse.FloatVar(&Options.Change.Ratio, "scale", 0, float64(1))
+	optparse.BoolVar(&Options.Change.Once, "once", 0, false)
+	optparse.BoolVar(&Options.Change.Reverse, "reverse", 0, false)
+	interval := optparse.String("interval", 'i', "")
 
-	flag.StringVar(&Options.Path, "path", "", "Path to root folder to generate files")
+	/* common options */
+	optparse.StringVar(&Options.Path, "path", 'p', "")
 
-	genType := flag.String("gen-type", "crypto", "Generator type. [(random), pseudo]")
-	seed := flag.Uint64("seed", 0, "Seed for data generator. Could be used with pseudo generator")
+	/* generator options */
+	genType := optparse.String("generator", 'g', "crypto")
+	seed := optparse.Uint("seed", 0, 0)
 
-	cmd := flag.String("cmd", "gen", "Command to execute. Could be [(gen, change)]")
+	/* auxillary options */
+	help := optparse.Bool("help", 'h', false)
+	version := optparse.Bool("version", 'v', false)
 
 	/* Parsing command-line */
-	flag.Parse()
+	args, err := optparse.Parse()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n\n", os.Args[0], err.Error())
+		usage(os.Stderr)
+		os.Exit(1)
+	}
+
+	/* processing command line arguments */
+	if *help {
+		usage(os.Stdout)
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Println("Version: 0.1.0")
+		os.Exit(0)
+	}
+
+	if len(args) == 0{
+		fmt.Fprintf(os.Stderr, "Error: Set command to use\n")
+		usage(os.Stderr)
+		os.Exit(1)
+	}
+
+	cmd := args[0]
+
 	processFileSize(*fileSize)
 	processInterval(*interval)
-	processCommand(*cmd)
-	processGeneratorType(*genType, *seed)
+	processCommand(cmd)
+	processGeneratorType(*genType, uint64(*seed))
 }
